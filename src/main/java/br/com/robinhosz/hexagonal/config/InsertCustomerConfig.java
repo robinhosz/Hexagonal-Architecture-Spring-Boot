875868f1822b5b4d2d37c7f858ce82e06442df0b
Jose Robson <joserobsonsiqueira23@hotmail.com>
2023-10-28 23:28:34 -0300

    feat: usercase bean configuration

---
+++ src/main/java/br/com/robinhosz/hexagonal/config/InsertCustomerConfig.java
@@ -0,0 +1,18 @@
+package br.com.robinhosz.hexagonal.config;
+
+import br.com.robinhosz.hexagonal.adapters.out.FindAddressByZipCodeAdapter;
+import br.com.robinhosz.hexagonal.adapters.out.InsertCustomerAdapter;
+import br.com.robinhosz.hexagonal.application.core.usecase.InsertCustomerUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InsertCustomerConfig {
+
+    @Bean
+    public InsertCustomerUseCase insertCustomerUseCase(FindAddressByZipCodeAdapter findAddressByZipCodeAdapter,
+    InsertCustomerAdapter insertCustomerAdapter) {
+        return new InsertCustomerUseCase(findAddressByZipCodeAdapter, insertCustomerAdapter);
+    }
+
+}
